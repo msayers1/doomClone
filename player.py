b4371ccc350596273e4a20c953b1b53e0fbcfde1
Michael Sayers <msayers1@gmail.com>
2024-08-17 11:23:50 -0400

    Initial Commit

---
+++ player.py
@@ -0,0 +1,18 @@
+form settings import *
+import pygame as pg
+import math
+
+class Player:
+    def __init__(self,game):
+        self.game = game
+        self.x, self.y = PLAYER_POS
+        self.angle = PLAYER_ANGLE
+    def movement(self):
+        pass
+    
+    def update(self):
+        self.movement()
+        
+    @property
+    def pos(self):
+        return self
---
+++ settings.py
@@ -0,0 +1,9 @@
+#game settings
+RES = width, height = 1600, 900
+FPS = 60
+
+
+PLAYER_POS = 1.5, 5 # mini map
+PLAYER_ANGLE = 0 # player direction1
+PLAYER_SPEED = .004 # Movement speed
+PLAYER_ROT_SPEED = 0.002 # Rotation Speed
