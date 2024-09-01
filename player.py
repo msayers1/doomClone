923bd5a18c4d7be6696ed0260c21cd6ce97e2ba9
Michael Sayers <msayers1@gmail.com>
2024-09-01 17:35:37 -0400

    16:31

--- player.py
+++ player.py
@@ -29,9 +29,8 @@ class Player:
             dx += -speed_sin
             dy += speed_cos
         
-        self.x += dx
-        self.y += dy
-
+        self.check_wall_collision(dx,dy)
+        
         if keys[pg.K_LEFT]:
             self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
         if keys[pg.K_RIGHT]:
@@ -39,8 +38,17 @@ class Player:
         self.angle %= math.tau
         # print(f"self angle {self.angle} and {keys}")
 
+    def check_wall(self, x, y):
+            return (x, y) not in self.game.map.world_map
+    
+    def check_wall_collision(self, dx, dy):
+            if self.check_wall(int(self.x + dx), int(self.y)):
+                self.x += dx
+            if self.check_wall(int(self.x), int(self.y + dy)):
+                self.y += dy
+
     def draw(self):
-        pg.draw.line(self.game.screen, 'yellow', (self.x * 100, self.y * 100), (self.x * 100 * WIDTH * math.cos(self.angle), self.y * 100 * WIDTH * math.sin(self.angle)), 2)
+        # pg.draw.line(self.game.screen, 'yellow', (self.x * 100, self.y * 100), (self.x * 100 * WIDTH * math.cos(self.angle), self.y * 100 * WIDTH * math.sin(self.angle)), 2)
         pg.draw.circle(self.game.screen, 'green', (self.x * 100, self.y * 100), 15)
     def update(self):
         self.movement()
--- settings.py
+++ settings.py
@@ -1,5 +1,9 @@
+import math
 #game settings
 RES = WIDTH, HEIGHT = 1600, 900
+HALF_WIDTH = WIDTH // 2
+HALF_HEIGHT = HEIGHT // 2
+
 FPS = 60
 
 
@@ -7,3 +11,13 @@ PLAYER_POS = 1.5, 5 # mini map
 PLAYER_ANGLE = 0 # player direction1
 PLAYER_SPEED = .004 # Movement speed
 PLAYER_ROT_SPEED = 0.002 # Rotation Speed
+
+FOV = math.pi / 3
+HALF_FOV = FOV / 2
+NUM_RAYS = WIDTH // 2
+HALF_NUM_RAYS = NUM_RAYS // 2
+DELTA_ANGLE = FOV / NUM_RAYS
+MAX_DEPTH = 20
+
+SCREEN_DIST = HALF_WIDTH / math.tan(HALF_FOV)
+SCALE = WIDTH // NUM_RAYS
