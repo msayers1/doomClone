8c09fdd9712040507b7e80b3aa9164cae1cf1be8
Michael Sayers <msayers1@gmail.com>
2025-04-05 16:53:46 -0400

    update

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ class Game:
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
         self.delta_time = 1
+        self.global_trigger = False
+        self.global_event = pg.USEREVENT + 0
+        pg.time.set_timer(self.global_event, 40)
         self.new_game()
         
     
@@ -51,11 +54,14 @@ class Game:
 
         
     def check_events(self):
+        self.global_trigger = False
         for event in pg.event.get():
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 print(f"Quitting....")
                 pg.quit()
                 sys.exit()
+            elif event.type == self.global_event:
+                self.global_trigger = True
             self.player.single_fire_event(event)
             # if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
                 
